ca5e2b052ddcddb22775f9126b54edc17a1d021c
Parceo2016416 <parceo-2016416@kinal.edu.gt>
2021-04-17 11:32:38 -0600

    Initial Structure

---
+++ src/servicios/jwt.js
@@ -0,0 +1,16 @@
+'use strict'
+var jwt = require('jwt-simple')
+var moment = require('moment');
+var secret = 'clave_secreta';
+
+exports.createToken = function(usuario){
+var playload = {
+    sub: usuario._id,
+    usuario: usuario.user,
+    rol: usuario.rol,
+
+    iat: moment().unix(),
+    exp: moment().day(10, 'days').unix() 
+}
+    return jwt.encode(payload,secret)
+}
